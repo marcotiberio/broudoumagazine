aff778097e517e0213b2d3b84c0e6aa7c333bdfc
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2021-12-17 10:34:16 +0100

    homepage link

--- Components/BlockHomepage/functions.php
+++ Components/BlockHomepage/functions.php
@@ -18,21 +18,33 @@ function getACFLayout()
                 'endpoint' => 0,
             ],
             [
-                'label' => __('Info', 'flynt'),
-                'name' => 'contentHtml',
-                'type' => 'wysiwyg',
-                'delay' => 1,
-                'media_upload' => 0,
-                'required' => 0,
+                'label' => __('Page Link - Title', 'flynt'),
+                'name' => 'pageLinkTitle',
+                'type' => 'text',
+                'required' => 1
             ],
             [
-                'label' => __('About', 'flynt'),
-                'name' => 'contentAboutHtml',
-                'type' => 'wysiwyg',
-                'delay' => 1,
-                'media_upload' => 0,
-                'required' => 0,
-            ]
+                'label' => __('Page Link - Button', 'flynt'),
+                'name' => 'pageLinkButton',
+                'type' => 'page_link',
+                'required' => 1
+            ],
+            // [
+            //     'label' => __('Info', 'flynt'),
+            //     'name' => 'contentHtml',
+            //     'type' => 'wysiwyg',
+            //     'delay' => 1,
+            //     'media_upload' => 0,
+            //     'required' => 0,
+            // ],
+            // [
+            //     'label' => __('About', 'flynt'),
+            //     'name' => 'contentAboutHtml',
+            //     'type' => 'wysiwyg',
+            //     'delay' => 1,
+            //     'media_upload' => 0,
+            //     'required' => 0,
+            // ]
         ]
     ];
 }
